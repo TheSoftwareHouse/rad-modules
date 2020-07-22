95725f85bd248fbb2939aa6370cb3f000a171683
eMKoded <emkoded@gmail.com>
2020-07-22 15:57:54 +0200

    Swagger docs for get-services

--- services/scheduler/src/app/features/scheduling/actions/get-services.action.ts
+++ services/scheduler/src/app/features/scheduling/actions/get-services.action.ts
@@ -19,14 +19,45 @@ export const getServicesActionValidation = celebrate(
  *
  * /api/scheduling/get-services:
  *   get:
- *     description: desc
+ *     tags:
+ *       - Scheduling
+ *     security: []
+ *     summary: Returns all services and actions that can be run by scheduler
  *     responses:
- *       201:
- *         description: desc
+ *       200:
+ *         description: Available services and actions
+ *         example: [ { "service": "security", "actions": [ "getPolicies", "getUsers" ]}, { "service": "mailer", "actions": ["send"] } ]
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   service:
+ *                     type: string
+ *                   actions:
+ *                     type: array
+ *                     items:
+ *                       type: string
  *       400:
- *         description: Validation Error
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref:  "#/definitions/BadRequestError"
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref:  "#/definitions/UnauthorizedError"
  *       500:
  *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref:  "#/definitions/InternalServerError"
  */
 export const getServicesAction = ({ commandBus }: GetServicesActionProps) => (
   req: Request,
