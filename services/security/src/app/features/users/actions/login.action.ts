fc6db0dc1dbb02e367b8476e79e877b2332de0fc
Marcin Kopa <marcin.info@gmail.com>
2022-10-08 08:20:01 +0200

    Added correct swagger description for login and logout endpoints

--- services/security/src/app/features/users/actions/login.action.ts
+++ services/security/src/app/features/users/actions/login.action.ts
@@ -19,14 +19,13 @@ export const loginActionValidation = celebrate(
 
 /**
  * @swagger
- *
  * /api/users/login:
  *   post:
  *     tags:
  *       - Users
  *     security: []
  *     summary: Login to app
- *     requestBody: &loginRequest
+ *     requestBody:
  *       required: true
  *       content:
  *         application/json:
@@ -41,7 +40,7 @@ export const loginActionValidation = celebrate(
  *                type: string
  *                required: true
  *                example: superadmin
- *     responses: &loginResponses
+ *     responses:
  *       200:
  *        description: auth success
  *        content:
@@ -75,14 +74,65 @@ export const loginActionValidation = celebrate(
  *                 error:
  *                   type: string
  *                   example: Wrong username or password
+ */
+
+/**
+ * @swagger
  * /api/public/auth/login:
  *   post:
  *     tags:
  *       - Public
  *     security: []
  *     summary: Login to app
- *     requestBody: *loginRequest
- *     responses: *loginResponses
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                required: true
+ *                example: superadmin
+ *              password:
+ *                type: string
+ *                required: true
+ *                example: superadmin
+ *     responses:
+ *       200:
+ *        description: auth success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                accessToken:
+ *                  type: string
+ *                  example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyNzQ4Zjc0MC1lOWRiLTQxYTMtOGJlNy04OTJlMWY5MzViYzgiLCJ1c2VybmFtZSI6InN1cGVyYWRtaW4iLCJhdHRyaWJ1dGVzIjpbIlJPTEVfU1VQRVJBRE1JTiJdLCJwb2xpY3kiOltdLCJ0eXBlIjoidXNlciIsImlhdCI6MTU4MzQ5ODIxNCwiZXhwIjoxNTgzNTA4MjE0fQ.LU3DKnwjzmqIMlxni0bwFNH4-MpjRp6dvt7cXztvQIc
+ *                refreshToken:
+ *                  type: string
+ *                  example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyNzQ4Zjc0MC1lOWRiLTQxYTMtOGJlNy04OTJlMWY5MzViYzgiLCJ1c2VybmFtZSI6InN1cGVyYWRtaW4iLCJhdHRyaWJ1dGVzIjpbIlJPTEVfU1VQRVJBRE1JTiJdLCJwb2xpY3kiOltdLCJ0eXBlIjoidXNlciIsImlhdCI6MTU4MzQ5ODIxNCwiZXhwIjoxNTgzNDk5MTE0fQ.tUSn6ktZRB8z1X7Q7QiMtQK7JFGI-AKJMcltlZaOR0U
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: child "username" fails because ["username" is not allowed to be empty]
+ *       401:
+ *         description: Unauthorized Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Wrong username or password
  */
 export const loginAction =
   ({ commandBus }: LoginActionProps) =>
